677d943b4aabad699524f19917ae09ff2982f9da
lukeKING69 <eaaluah@students.eaaa.dk>
2025-07-14 12:59:12 +0200

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -36,12 +36,6 @@ export const HeroSection = () => {
             Software developer with a passion for learning, building, and
             solving real problems.
           </p>
-
-          <div className="pt-4 opacity-0 animate-fade-in-delay-4">
-            <a href="#projects" className="button">
-              View My Work
-            </a>
-          </div>
         </div>
       </div>
 
